964f56ea7c44faf3be9947bcbd983fa4f35b1272
AbinAntony8888 <119113517+AbinAntony8888@users.noreply.github.com>
2024-10-01 09:16:20 +0530

    design changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="m-4 mb-5">
-      <div className="w-full h-screen border-2 border-gray-400 flex flex-row p-2">
+      <div className="w-full h-screen border-2 border-gray-400 flex flex-row p-1 bg-gray-300">
         {/* bg-gradient-to-br from-white to-violet-800 */}
         <List/>
         <Chat/>
